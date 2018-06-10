9636996db34ffde285d572e5721026a17f04fe06
Garrett Levine <garrlevine@gmail.com>
2018-06-10 18:57:26 -0400

    adds scrips for model

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -17,6 +17,7 @@ class App extends React.Component<Props, State> {
 
     this.state = {
       message: '',
+
     };
   }
 
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,11 +1,8 @@
-// @flow
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(
   <App header="great" />,
-  // $FlowFixMe: suppressing this error until figure out
   document.getElementById('app'), // eslint-disable-line
 );
